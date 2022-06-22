936a370aa55f76cbbb011ff6b9e78093cc43c758
Olivus <107968542+OlivusFlr@users.noreply.github.com>
2022-06-21 19:44:06 -0600

    Update and rename a342006-P4.Semáforo_vial_con_peatonal.c to Semáforo_vial_con_peatonal.c

--- Semáforo_vial_con_peatonal.c
+++ Semáforo_vial_con_peatonal.c
@@ -1,13 +1,3 @@
-/* 
-Universidad Autonoma de Chihuahua
-Facultad de Medicina y Ciencias Biomédicas
-Ingeniería Biomédica
-Grupo: 5IB1
-No de práctica: 4
-Nombre de la práctica: Semáforo vial con peatonal
-Equipo: 6
-Docente: Ing. Juan Carlos Osorio Diaz 
-*/
 #include <18f2550.H>
 #fuses INTRC, NOMCLR, NOBROWNOUT
 #use delay (internal = 8M)
